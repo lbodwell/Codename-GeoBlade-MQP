92a8be1a12c572b6c0e5097905f22065437ed6a1
Luke Bodwell <ljbodwell@wpi.edu>
2021-09-29 15:34:30 -0400

    Interactive music tests.

--- GeoBlade/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ GeoBlade/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,13 +13,32 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID LEVEL1_MUSIC = 2026953958U;
         static const AkUniqueID PLAYER_FOOTSTEP = 2453392179U;
         static const AkUniqueID PLAYER_JUMP = 1305133589U;
     } // namespace EVENTS
 
+    namespace STATES
+    {
+        namespace MUSIC_LEVEL1_TRACK1
+        {
+            static const AkUniqueID GROUP = 2489350135U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SECTION1 = 3524678539U;
+                static const AkUniqueID SECTION2 = 3524678536U;
+                static const AkUniqueID SECTION3 = 3524678537U;
+            } // namespace STATE
+        } // namespace MUSIC_LEVEL1_TRACK1
+
+    } // namespace STATES
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID PLAYER = 1069431850U;
     } // namespace BANKS
 
